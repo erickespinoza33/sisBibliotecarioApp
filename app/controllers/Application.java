3487c9d7652a143108eeded43446c18866b0689c
erickespinoza33 <erickespinoza27@gmail.com>
2014-06-04 23:33:19 -0600

    -modificacion del template

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,15 +2,25 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-
 import java.util.*;
-
 import models.*;
 
 public class Application extends Controller {
 
     public static void index() {
-        render();
+    	Institucion institucion = new Institucion();
+    	institucion.setNombreInstitucion("UCA");
+    	institucion.setDireccionInstitucion("La Libertad");
+    	institucion.setTelefonoInstitucion("2222-7777");
+    	
+    	Institucion institucion1 = new Institucion();
+    	institucion1.setNombreInstitucion("UES");
+    	institucion1.setDireccionInstitucion("San Salvador");
+    	institucion1.setTelefonoInstitucion("2222-7777");
+        render(institucion, institucion1);
     }
-
+    
+    public static void signin(){
+    	render();
+    }
 }
